99a7cace71a6787b0ddceda1a0b97ae22ef603ec
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2023-04-05 04:48:34 +0400

    updated deployment script for mumbai deployment

--- 2wheels2rent/config/firebaseAdmin.ts
+++ 2wheels2rent/config/firebaseAdmin.ts
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import secret from "../secret.json";
+//import secret from "../secret.json";
 
 const clientEmail = process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL;
 const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
@@ -7,10 +7,10 @@ const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      privateKey: secret.private_key,
-      //privateKey: process.env.FIREBASE_ADMIN
-      //  ? process.env.FIREBASE_ADMIN.replace(/\\n/gm, "\n")
-      //  : undefined,
+      //privateKey: secret.private_key,
+      privateKey: process.env.FIREBASE_ADMIN
+        ? process.env.FIREBASE_ADMIN.replace(/\\n/gm, "\n")
+        : undefined,
       clientEmail,
       projectId,
     }),
--- truffle/migrations/01-migration.js
+++ truffle/migrations/01-migration.js
@@ -64,49 +64,12 @@ module.exports = async (deployer, network, accounts) => {
       "MaticW2Rdex address set as authorized Minter and Burner in LP token"
     );
 
-    // if Ganache, add liquidity to the DEX
-    if (network === "development") {
-      // Transfer 1,000 W2R tokens to the DEX
-      const transferMatic = web3.utils.toWei("100", "ether");
-      const transferW2R = web3.utils.toWei("1000", "ether");
-      // account[0] approve W2R transfer to DEX
-      await W2RInstance.approve(MaticW2RdexInstance.address, transferW2R, {
-        from: deployerAccount,
-      });
-      // account[0] launch addLiquidity function
-      await MaticW2RdexInstance.addLiquidity(transferW2R, {
-        from: deployerAccount,
-        value: transferMatic,
-      });
-      console.log("1,000 W2R tokens and 100 MATIC transferred to MaticW2Rdex");
-      // LP token Total supply
-      const LPtokenTotalSupply = await MaticW2RPairTokenInstance.totalSupply();
-      console.log(
-        "LP token total supply: ",
-        web3.utils.fromWei(LPtokenTotalSupply, "ether")
-      );
-      const dexW2RBalance = await W2RInstance.balanceOf(
-        MaticW2RdexInstance.address
-      );
-      console.log(
-        "MaticW2Rdex balance: ",
-        web3.utils.fromWei(dexW2RBalance, "ether")
-      );
-      const dexMaticBalance = await web3.eth.getBalance(
-        MaticW2RdexInstance.address
-      );
-      console.log(
-        "MaticW2Rdex balance: ",
-        web3.utils.fromWei(dexMaticBalance, "ether")
-      );
-      const deployerLPBalance = await MaticW2RPairTokenInstance.balanceOf(
-        deployerAccount
-      );
-      console.log(
-        "deployer LP balance: ",
-        web3.utils.fromWei(deployerLPBalance, "ether")
-      );
-    }
+    // add DEX address to VaultW2R approved addresses
+    await VaultW2RInstance.setApprovedContract(
+      MaticW2RdexInstance.address,
+      true
+    );
+    console.log("DEX address set in VaultW2R contract");
 
     // Deploy TwoWheels2RentLender NFT contract
     await deployer.deploy(TwoWheels2RentLender);
@@ -176,31 +139,69 @@ module.exports = async (deployer, network, accounts) => {
     await TwoWheels2RentRenterInstance.setIpfsHash(renterIPFS);
     console.log("IPFS hash set in TwoWheels2RentRenter NFT contract");
 
-    // add DEX address to VaultW2R approved addresses
-    await VaultW2RInstance.setApprovedContract(
-      MaticW2RdexInstance.address,
-      true
-    );
-    console.log("DEX address set in VaultW2R contract");
-
     // set whitelist contracts addresses in VaultW2R as approvers
     await VaultW2RInstance.setWhitelistLenders(LenderWhitelistInstance.address);
     console.log("LenderWhitelist address set in VaultW2R contract");
     await VaultW2RInstance.setWhitelistRenters(RenterWhitelistInstance.address);
     console.log("RenterWhitelist address set in VaultW2R contract");
 
-    // transfer 100000 W2R from account[0] to account[1]
-    const transferAmount2 = web3.utils.toWei("100000", "ether");
-    await W2RInstance.transfer(accounts[1], transferAmount2, {
-      from: deployerAccount,
-    });
-    console.log("100000 W2R tokens transferred to account[1]");
+    /////////////////////////////////////////////////////////////////////////////////
 
-    // transfer 100000 W2R from account[0] to account[2]
-    await W2RInstance.transfer(accounts[2], transferAmount2, {
-      from: deployerAccount,
-    });
-    console.log("100000 W2R tokens transferred to account[2]");
+    // if Ganache, add liquidity to the DEX
+    if (network === "development") {
+      // Transfer 1,000 W2R tokens to the DEX
+      const transferMatic = web3.utils.toWei("100", "ether");
+      const transferW2R = web3.utils.toWei("1000", "ether");
+      // account[0] approve W2R transfer to DEX
+      await W2RInstance.approve(MaticW2RdexInstance.address, transferW2R, {
+        from: deployerAccount,
+      });
+      // account[0] launch addLiquidity function
+      await MaticW2RdexInstance.addLiquidity(transferW2R, {
+        from: deployerAccount,
+        value: transferMatic,
+      });
+      console.log("1,000 W2R tokens and 100 MATIC transferred to MaticW2Rdex");
+      // LP token Total supply
+      const LPtokenTotalSupply = await MaticW2RPairTokenInstance.totalSupply();
+      console.log(
+        "LP token total supply: ",
+        web3.utils.fromWei(LPtokenTotalSupply, "ether")
+      );
+      const dexW2RBalance = await W2RInstance.balanceOf(
+        MaticW2RdexInstance.address
+      );
+      console.log(
+        "MaticW2Rdex balance: ",
+        web3.utils.fromWei(dexW2RBalance, "ether")
+      );
+      const dexMaticBalance = await web3.eth.getBalance(
+        MaticW2RdexInstance.address
+      );
+      console.log(
+        "MaticW2Rdex balance: ",
+        web3.utils.fromWei(dexMaticBalance, "ether")
+      );
+      const deployerLPBalance = await MaticW2RPairTokenInstance.balanceOf(
+        deployerAccount
+      );
+      console.log(
+        "deployer LP balance: ",
+        web3.utils.fromWei(deployerLPBalance, "ether")
+      );
+      // transfer 100000 W2R from account[0] to account[1]
+      const transferAmount2 = web3.utils.toWei("100000", "ether");
+      await W2RInstance.transfer(accounts[1], transferAmount2, {
+        from: deployerAccount,
+      });
+      console.log("100000 W2R tokens transferred to account[1]");
+
+      // transfer 100000 W2R from account[0] to account[2]
+      await W2RInstance.transfer(accounts[2], transferAmount2, {
+        from: deployerAccount,
+      });
+      console.log("100000 W2R tokens transferred to account[2]");
+    }
   } catch (error) {
     console.log(error);
   }
